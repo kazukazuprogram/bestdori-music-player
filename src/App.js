551254cb158f6503bafcbb86d8c90bd560b967c5
kazukazuprogram <A>
2020-11-11 00:39:47 +0900

    dev

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import { Component } from "react";
 import "./App.css"
+import next from "./img/next.svg"
+import pause from "./img/pause.svg"
+import play from "./img/play.svg"
+import prev from "./img/prev.svg"
+import loop from "./img/loop.svg"
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       songdata: "loading",
-      banddata: []
+      banddata: [],
+      seek: 1,
+      playing: -2
+      /*
+      -2: 初期
+      -1: ローディング
+      0: 未割り当て
+      1～: ID
+      */
     }
+    this.audio = new Audio()
+    this.audio.ontimeupdate = e=>this.setState({seek: e.target.currentTime / e.target.duration})
+  }
+
+  play(id) {
+    var idstr = ("00" + id).slice(-3)
+    console.log(id)
+    this.audio.src = `https://bestdori.com/assets/jp/sound/bgm${idstr}_rip/bgm${idstr}.mp3`
+    console.log("loading")
+    this.setState({playing: -1})
+    this.audio.addEventListener("canplay", e=>{
+      console.log("play")
+      e.target.play()
+      this.setState({playing: id})
+    })
   }
 
   componentDidMount() {
@@ -17,26 +45,30 @@ export default class App extends Component {
       fetch('https://bestdori.com/api/songs/all.5.json')
       .then(response => response.json())
       .then(data => {
-        console.log(banddata)
-        console.log(data)
         var list = []
         for (var d in data) {
-          console.log(d)
+          if (d.length >= 4) continue
           const f = Math.ceil(d/10)*10
           const jack = "https://bestdori.com/assets/jp/musicjacket/musicjacket"+f+"_rip/assets-star-forassetbundle-startapp-musicjacket-musicjacket"+f+"-"+data[d].jacketImage[0]+"-jacket.png"
+          var musicTitle = data[d].musicTitle[0]||data[d].musicTitle[1]
           list.push((
-            <dev className="songcard">
-              <div className="left">
-                <img src={jack}/>
-              </div>
-              <div className="right">
-                <div className="musicTitle">{data[d].musicTitle[0]||data[d].musicTitle[1]}</div>
-                <div className="bandName">{banddata[data[d].bandId].bandName[0]}</div>
+            <dev
+              className="songcard"
+            >
+              <div className="over" data-id={d} onClick={e=>this.play(e.target.getAttribute("data-id"))}/>
+              <div className="backContainer">
+                <div className="left">
+                  <img src={jack} loading="lazy" alt={`${musicTitle} jacketimage`}/>
+                </div>
+                <div className="right">
+                  <div className="musicTitle">{musicTitle}</div>
+                  <div className="bandName">{banddata[data[d].bandId].bandName[0]}</div>
+                </div>
               </div>
             </dev>
           ))
         }
-        this.setState({songdata: list})
+        this.setState({songdata: list.reverse()})
       });
     })
   }
@@ -48,7 +80,19 @@ export default class App extends Component {
         <div className="Container">
           {this.state.songdata}
         </div>
-        <div className="Footer"></div>
+        <div className="Footer">
+          <div className={"seekbar " + (this.state.playing!==-1||"loading")} style={{
+            width: `${this.state.seek * 100}%`,
+            "background-color": this.state.playing===-1&&"gray"||"red"
+          }}/>
+          <div className="controller">
+            <img className="prev" src={prev} />
+            <img className="pause" src={pause} />
+            <img className="play" src={play} />
+            <img className="next" src={next} />
+            <img className="loop" src={loop} />
+          </div>
+        </div>
       </div>
     );
   }
