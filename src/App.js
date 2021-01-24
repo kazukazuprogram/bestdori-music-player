12ef257076917fb1cfac383784c7f7270462fab7
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2021-01-25 00:29:06 +0900

    とりあえず再生できる

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ export default class App extends Component {
     super(props)
     this.state = {
       songdata: "loading",
-      banddata: []
+      banddata: [],
     }
+    this.audio = new Audio()
+    this.audio.oncanplay = ()=>{this._play()}
   }
 
   componentDidMount() {
@@ -17,15 +19,17 @@ export default class App extends Component {
       fetch('https://bestdori.com/api/songs/all.5.json')
       .then(response => response.json())
       .then(data => {
-        console.log(banddata)
-        console.log(data)
         var list = []
         for (var d in data) {
-          console.log(d)
           const f = Math.ceil(d/10)*10
           const jack = "https://bestdori.com/assets/jp/musicjacket/musicjacket"+f+"_rip/assets-star-forassetbundle-startapp-musicjacket-musicjacket"+f+"-"+data[d].jacketImage[0]+"-jacket.png"
           list.push((
-            <dev className="songcard">
+            <dev className={"songcard"}>
+              <div className={"overlay music-" + d}
+                onClick={(e)=>{
+                  this.play(e.target.className.match(/ music-([0-9]+)/)[1])
+                }}
+              ></div>
               <div className="left">
                 <img src={jack}/>
               </div>
@@ -39,6 +43,21 @@ export default class App extends Component {
         this.setState({songdata: list})
       });
     })
+  }
+
+  play(id) {
+    var id_3 = ("00"+id).slice(-3)
+    var url = "https://bestdori.com/assets/jp/sound/bgm"+id_3+"_rip/bgm"+id_3+".mp3"
+    if (this.audio.currentSrc == url) {
+      return this._play()
+    } else {
+      this.audio.src = url
+      this.audio.load()
+    }
+  }
+
+  _play() {
+    this.audio.play()
   }
 
   render() {
